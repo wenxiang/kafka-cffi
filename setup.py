37f19452af6aec5b871afcf91e81b12580b0b271
Wenxiang Wu <wwu@zendesk.com>
2018-07-18 09:06:59 +0800

    update setup and travis.yml

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ install_requires=["cffi>=1.0.0", "six"]
 dirname = os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="kafka_cffi",
-    version="0.1",
+    name="kafka-cffi",
+    version="0.11.4b1",
     description="A CFFI binding for librdkafka",
     author="Wenxiang Wu",
-    author_email="wwu@zendesk.com",
-    url="https://github.com/zendesk/cffi_kafka",
+    author_email="thewrongboy@gmail.com",
+    url="https://github.com/wenxiang/cffi_kafka",
     license="MIT",
     package_dir={"": "src"},
     packages=["kafka_cffi"],
