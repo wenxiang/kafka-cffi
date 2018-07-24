62b8ccffa7f1ff6f8a9050d628b9df82d9fde5eb
Wenxiang Wu <thewrongboy@gmail.com>
2018-07-24 02:40:51 +0000

    progress

---
+++ scripts/build-wheels.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CFLAGS="-I/opt/build-libs/include"
+export LDFLAGS="-L/opt/build-libs/lib"
+
+/opt/pypy-6.0.0/bin/pypy setup.py sdist
+for env in $PYENVS; do
+  /opt/$env/bin/pypy setup.py bdist_wheel
+done
+
+for whl in $(ls dist/*.whl); do
+  LD_LIBRARY_PATH=/opt/build-libs/lib \
+  /opt/pypy3.5-6.0.0/bin/auditwheel repair $whl
+done
