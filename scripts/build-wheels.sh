d567e1c157dada5399a5301418ecc47b8921aac1
Chow Loong Jin <hyperair@debian.org>
2019-10-03 14:49:05 +0800

    Check git status inside container

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo 'Checking git status inside the container'
+git status
+
+echo 'Checking git describe'
+git describe
+
 for PYBIN in /opt/python/*/bin; do
+  echo "Checking version with $PYBIN/pypy"
+  $PYBIN/pypy setup.py --version
   $PYBIN/pypy setup.py bdist_wheel -d pypy-dist
 done
 
