8dcc014b688cda52e887061da9ee18c570691843
Chow Loong Jin <hyperair@debian.org>
2020-06-09 15:11:40 +0800

    Set openssl prefix to /usr/local/ so librdkafka actually finds it

--- scripts/install-libs.sh
+++ scripts/install-libs.sh
@@ -11,14 +11,14 @@ if [[ ! -e /usr/local/lib/librdkafka.so ]]; then
   mkdir /tmp/openssl && cd $_ && \
     curl -L https://www.openssl.org/source/openssl-$OPENSSL_VER.tar.gz | \
     tar xz --strip-components=1 && \
-    ./config zlib no-krb5 zlib shared && \
-    make && make install && \
+    ./config --prefix=/usr/local zlib no-krb5 zlib shared && \
+    make && make install && ldconfig && \
     rm -rf /tmp/openssl
 
   mkdir /tmp/librdkafka && cd $_ && \
     curl -L https://github.com/edenhill/librdkafka/archive/v$LIBRDKAFKA_VERSION.tar.gz | \
     tar xz --strip-components=1 && \
-    ./configure && make && make install && \
+    ./configure && make && make install && ldconfig && \
     rm -rf /tmp/librdkafka
 else
   echo "librdkafka.so exists, skipping library install"
